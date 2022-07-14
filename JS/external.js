9456cc24775cc2939a5fb99fef209faef5f9c23c
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2022-07-14 22:56:04 +0530

    JavaScript

---
+++ JS/external.js
@@ -0,0 +1,10 @@
+// Add HTML Tag in JavaScript
+document.write("<b> <i>HTML Tag in JavaScript.<br><br> </i> </b>");
+document.write("This an Example of External JavaSript.<br><br>");
+
+
+
+/*  comments in JavaScript
+    1. // single line comment:
+    2. multiple line comment:
+*/
